8ad8efd0cdbaede6566b4ca7e3be6cffebeaaaf0
xenotux <xenotux@outlook.com>
2021-05-20 14:33:33 +0200

    Add mana.dll, Copy files from cmake for sample build

--- cmake/sample.cmake
+++ cmake/sample.cmake
@@ -2,3 +2,7 @@ file(GLOB_RECURSE Sample.File.SRC sample/src/*.cpp sample/src/*.c)
 
 add_executable(mana-sample ${Sample.File.SRC})
 target_link_libraries(mana-sample mana)
+
+# Copy mana dll and assets folder to binary dir
+file(COPY engine/src/csharp/mana.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+file(COPY sample/assets DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
