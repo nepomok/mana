3145e6edc80e3a34e234ac416cba394caf7805db
xenotux <xenotux@outlook.com>
2021-10-14 20:30:37 +0200

    Draw texture face normals in debug pass

--- engine/src/render/3d/passes/debugpass.cpp
+++ engine/src/render/3d/passes/debugpass.cpp
@@ -33,11 +33,11 @@ layout (location = 6) in vec4 instanceRow1;
 layout (location = 7) in vec4 instanceRow2;
 layout (location = 8) in vec4 instanceRow3;
 
-out VS_OUT {
+out Vertex {
     vec4 vPos;
     vec3 vNorm;
-    vec3 vTang;
-    vec3 vBitang;
+    vec3 vTan;
+    vec3 vBitan;
     vec2 vUv;
 } vs_out;
 
@@ -55,8 +55,8 @@ void main()
 {
     vs_out.vPos = vec4(position, 1);
     vs_out.vNorm = normal;
-    vs_out.vTang = tangent;
-    vs_out.vBitang = bitangent;
+    vs_out.vTan = tangent;
+    vs_out.vBitan = bitangent;
     vs_out.vUv = uv;
     gl_Position = vs_out.vPos;
 }
@@ -65,13 +65,13 @@ void main()
 static const char *SHADER_GEOMETRY = R"###(#version 460 core
 
 layout(triangles) in;
-layout(line_strip, max_vertices = 18) out;
+layout(line_strip, max_vertices = 218) out;
 
-in VS_OUT {
+in Vertex {
     vec4 vPos;
     vec3 vNorm;
-    vec3 vTang;
-    vec3 vBitang;
+    vec3 vTan;
+    vec3 vBitan;
     vec2 vUv;
 } gs_in[];
 
@@ -90,86 +90,100 @@ uniform Globals globals;
 
 uniform sampler2D normal;
 
-void main()
+void drawLine(vec4 start, vec4 end)
+{
+    pos = start;
+    gl_Position = pos;
+    EmitVertex();
+    pos = end;
+    gl_Position = pos;
+    EmitVertex();
+    EndPrimitive();
+}
+
+void drawTextureNormal(vec4 vPos,
+                vec3 vNorm,
+                vec3 vTan,
+                vec3 vBitan,
+                vec2 vUv)
 {
     mat4 mvp = globals.PROJECTION * globals.VIEW * globals.MODEL;
 
+    // Draw texture normals at vertex positions, normals for fragments between vertices are not drawn
+    vec3 T = normalize(vTan);
+    vec3 B = normalize(vBitan);
+    vec3 N = normalize(vNorm);
+
+    mat3 TBN = mat3(T, B, N);
+
+    vec3 tangentNormal = texture(normal, vUv).xyz;
+    tangentNormal = normalize((tangentNormal * 2) - 1);
+
+    //Transform tangent space normal into local space
+    vec3 localNormal = TBN * (tangentNormal * globals.scale);
+
+    //Transform local space normal into clip space
+    vec4 clipNormal = mvp * vec4((vec4(localNormal, 1) + vPos).xyz, 1);
+    vec4 clipPos =  mvp * vPos;
+
+    color = vec4(1, 1, 0, 1);
+    drawLine(clipPos, clipNormal);
+}
+
+void drawVertexNormal(vec4 vPos,
+                        vec3 vNorm,
+                        vec3 vTan,
+                        vec3 vBitan,
+                        vec2 vUv)
+{
+    mat4 mvp = globals.PROJECTION * globals.VIEW * globals.MODEL;
+
+    vec4 cPos = mvp * vPos;
+    vec4 cNorm = mvp * vec4((vec4(vNorm * globals.scale, 0) + vPos).xyz, 1);
+    vec4 cTang = mvp * vec4((vec4(vTan * globals.scale, 0) + vPos).xyz, 1);
+    vec4 cBitang = mvp * vec4((vec4(vBitan * globals.scale, 0) + vPos).xyz, 1);
+
+    // Draw vertex normal, tangent and bitangent
+    //Normal
+    color = vec4(0, 0, 1, 1);
+    drawLine(cPos, cNorm);
+
+    //Tangent
+    color = vec4(1, 0, 0, 1);
+    drawLine(cPos, cTang);
+
+    //Bitangent
+    color = vec4(0, 1, 0, 1);
+    drawLine(cPos, cBitang);
+}
+
+void drawInterpolatedVertex(vec2 position)
+{
+    vec4 vPos = mix(mix(gs_in[0].vPos, gs_in[1].vPos, position.x), gs_in[2].vPos, position.y);
+    vec3 vNorm = mix(mix(gs_in[0].vNorm, gs_in[1].vNorm, position.x), gs_in[2].vNorm, position.y);
+    vec3 vTan = mix(mix(gs_in[0].vTan, gs_in[1].vTan, position.x), gs_in[2].vTan, position.y);
+    vec3 vBitan = mix(mix(gs_in[0].vBitan, gs_in[1].vBitan, position.x), gs_in[2].vBitan, position.y);
+    vec2 vUv = mix(mix(gs_in[0].vUv, gs_in[1].vUv, position.x), gs_in[2].vUv, position.y);
+    drawTextureNormal(vPos, vNorm, vTan, vBitan, vUv);
+}
+
+void main()
+{
     //Draw normal direction vectors for each vertex in the triangle
     for(uint i = 0; i < 3; i++)
     {
-        vec4 vPos = gs_in[i].vPos;
-        vec3 vNorm = gs_in[i].vNorm;
-        vec3 vTang = gs_in[i].vTang;
-        vec3 vBitang = gs_in[i].vBitang;
-        vec2 vUv = gs_in[i].vUv;
+        drawVertexNormal(gs_in[i].vPos, gs_in[i].vNorm, gs_in[i].vTan, gs_in[i].vBitan, gs_in[i].vUv);
+    }
 
-        if (globals.hasNormalTexture == 1)
+    if (globals.hasNormalTexture == 1)
+    {
+        //Draw surface normals
+        for (int x = 0; x < 5; x++)
         {
-            // Draw texture normals at vertex positions, normals for fragments between vertices are not drawn
-            vec3 T = normalize(vTang);
-            vec3 B = normalize(vBitang);
-            vec3 N = normalize(vNorm);
-
-            mat3 TBN = mat3(T, B, N);
-
-            vec3 tangentNormal = texture(normal, vUv).xyz;
-            tangentNormal = normalize((tangentNormal * 2) - 1);
-
-            //Transform tangent space normal into local space
-            vec3 localNormal = TBN * (tangentNormal * globals.scale);
-
-            //Transform local space normal into clip space
-            vec4 clipNormal = mvp * vec4((vec4(localNormal, 1) + vPos).xyz, 1);
-            vec4 clipPos =  mvp * vPos;
-
-            color = vec4(1, 1, 0, 1);
-            pos = clipPos;
-            gl_Position = pos;
-            EmitVertex();
-
-            pos = clipNormal;
-            gl_Position = pos;
-            EmitVertex();
-
-            EndPrimitive();
-        }
-        else
-        {
-            vec4 cPos = mvp * vPos;
-            vec4 cNorm = mvp * vec4((vec4(vNorm * globals.scale, 0) + vPos).xyz, 1);
-            vec4 cTang = mvp * vec4((vec4(vTang * globals.scale, 0) + vPos).xyz, 1);
-            vec4 cBitang = mvp * vec4((vec4(vBitang * globals.scale, 0) + vPos).xyz, 1);
-
-            // Draw vertex normal, tangent and bitangent
-            //Normal
-            color = vec4(0, 0, 1, 1);
-            pos = cPos;
-            gl_Position = pos;
-            EmitVertex();
-            pos = cNorm;
-            gl_Position = pos;
-            EmitVertex();
-            EndPrimitive();
-
-            //Tangent
-            color = vec4(1, 0, 0, 1);
-            pos = cPos;
-            gl_Position = pos;
-            EmitVertex();
-            pos = cTang;
-            gl_Position = pos;
-            EmitVertex();
-            EndPrimitive();
-
-            //Bitangent
-            color = vec4(0, 1, 0, 1);
-            pos = cPos;
-            gl_Position = pos;
-            EmitVertex();
-            pos = cBitang;
-            gl_Position = pos;
-            EmitVertex();
-            EndPrimitive();
+            for (int y = 0; y < 5; y++)
+            {
+                drawInterpolatedVertex(vec2(x / 5.0f, y / 5.0f));
+            }
         }
     }
 }
@@ -225,7 +239,7 @@ namespace engine {
                                       false));
 
         if (drawNormals) {
-            for (auto &deferredCommand : scene.deferred) {
+            for (auto &deferredCommand: scene.deferred) {
                 if (!shader->setFloat("globals.scale", 0.1f))
                     throw std::runtime_error("");
                 shader->setMat4("globals.MODEL", deferredCommand.transform.model());
