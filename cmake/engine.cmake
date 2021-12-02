cb7b4fef5798fc377f47660549f2e5f3f7f0de27
xenotux <xenotux@outlook.com>
2021-12-02 23:46:32 +0100

    Add submodules, Increase cmake minimum version

--- cmake/engine.cmake
+++ cmake/engine.cmake
@@ -5,7 +5,6 @@ option(BUILD_ENGINE_COMPAT_IMGUI "Build the imgui compatibility and render pass 
 option(BUILD_ENGINE_SCRIPT_MONO "Build the mono script implementation" ON)
 option(BUILD_ENGINE_PHYSICS_BOX2D "Build the box2d physics implementation" ON)
 option(BUILD_ENGINE_AUDIO_OPENAL "Build the OpenAL audio implementation" ON)
-option(LINK_ENGINE_IM_GUI "Compile imgui into the engine library" ON)
 
 add_compile_definitions(BOX2D_VERSION=${BOX2D_VERSION})
 
@@ -14,15 +13,16 @@ set(Engine.Dir.SRC engine/src/)
 
 file(GLOB_RECURSE Engine.File.SRC ${Engine.Dir.SRC}*.cpp ${Engine.Dir.SRC}*.c)
 
-if (LINK_ENGINE_IM_GUI)
-    add_library(mana SHARED ${Engine.File.SRC} ${IMGUI.SRC})
-    add_compile_definitions(BUILD_ENGINE_IM_GUI)
-else ()
-    add_library(mana SHARED ${Engine.File.SRC})
-endif ()
+add_library(mana SHARED ${Engine.File.SRC})
 
 target_include_directories(mana PUBLIC ${Engine.Dir.INCLUDE})
 target_include_directories(mana PUBLIC ${Engine.Dir.SRC})
+
+if (BUILD_ENGINE_COMPAT_IMGUI)
+    include(cmake/imgui.cmake)
+    add_compile_definitions(BUILD_ENGINE_IM_GUI)
+    target_link_libraries(mana imgui) # imgui target provided by lib/imgui.cmake
+endif ()
 
 target_link_libraries(mana
         Threads::Threads
@@ -34,6 +34,11 @@ target_link_libraries(mana
         spirv-cross-core
         spirv-cross-glsl
         spirv-cross-hlsl)
+
+if (BUILD_ENGINE_AUDIO_OPENAL)
+    add_compile_definitions(BUILD_ENGINE_AUDIO_OPENAL)
+    target_link_libraries(mana openal)
+endif ()
 
 if (BUILD_ENGINE_DISPLAY_GLFW)
     add_compile_definitions(BUILD_ENGINE_DISPLAY_GLFW)
@@ -53,17 +58,12 @@ if (BUILD_ENGINE_COMPAT_IMGUI)
     add_compile_definitions(BUILD_ENGINE_COMPAT_IMGUI)
 endif ()
 
-if(BUILD_ENGINE_SCRIPT_MONO)
+if (BUILD_ENGINE_SCRIPT_MONO)
     add_compile_definitions(BUILD_ENGINE_SCRIPT_MONO)
     target_link_libraries(mana mono-2.0)
-endif()
+endif ()
 
 if (BUILD_ENGINE_PHYSICS_BOX2D)
     add_compile_definitions(BUILD_ENGINE_PHYSICS_BOX2D)
     target_link_libraries(mana ${BOX2D_LIB})
-endif()
-
-if (BUILD_ENGINE_AUDIO_OPENAL)
-    add_compile_definitions(BUILD_ENGINE_AUDIO_OPENAL)
-    target_link_libraries(mana openal)
-endif()
+endif ()
