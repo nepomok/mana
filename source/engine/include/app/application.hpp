d83ca3a81403d652315a759d1a1ecbe61fbcb880
xenotux <xenotux@outlook.com>
2022-01-06 19:39:55 +0100

    Fix catch all exception handler

--- source/engine/include/app/application.hpp
+++ source/engine/include/app/application.hpp
@@ -91,25 +91,6 @@ namespace engine {
 
         virtual int loop() {
 //Disable catch all exception handler in debug build to be able to use debugger exception handler
-/**
- *  Mana - 3D Game Engine
- *  Copyright (C) 2021  Julian Zampiccoli
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
 #ifndef MANA_DEBUG_BUILD
             try {
 #endif
@@ -123,25 +104,6 @@ namespace engine {
                 deltaTime = static_cast<float>(frameDelta.count()) / 1000000000.0f;
             }
             stop();
-/**
- *  Mana - 3D Game Engine
- *  Copyright (C) 2021  Julian Zampiccoli
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
 #ifndef MANA_DEBUG_BUILD
             }
             catch (const std::exception &e) {
@@ -155,7 +117,7 @@ namespace engine {
                 while (true) {
                     window->update();
 
-                    ImGuiCompat::NewFrame(*window);
+                    ImGuiCompat::NewFrame(*window, graphicsBackend);
                     ImGui::NewFrame();
 
                     ImGui::Begin(("Uncaught Exception " + timeStr).c_str());
@@ -173,7 +135,7 @@ namespace engine {
                     ImGui::End();
 
                     ImGui::Render();
-                    ImGuiCompat::DrawData(*window, window->getRenderTarget());
+                    ImGuiCompat::DrawData(*window, window->getRenderTarget(graphicsBackend), graphicsBackend);
 
                     window->swapBuffers();
                 }
